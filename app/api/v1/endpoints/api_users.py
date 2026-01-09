23cd835f786f16fe13045e1d46aa5455a5b4f391
eric23489 <eric23489@gmail.com>
2026-01-09 11:41:22 +0800

    modify location of dependencies.py, and README.MD

--- app/api/v1/endpoints/api_users.py
+++ app/api/v1/endpoints/api_users.py
@@ -3,7 +3,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.core.security import create_access_token
-from app.api.v1.endpoints.auth import get_current_user
+from app.core.dependencies import get_current_user
 from app.db.session import get_db
 from app.schemas.user import Token, UserCreate, UserResponse
 from app.services.user_service import UserService
@@ -24,7 +24,7 @@ def login(
     user = UserService(db).authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="Incorrect email or password",
         )
     token = create_access_token({"sub": user.email})
